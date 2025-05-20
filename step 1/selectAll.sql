6c92e813653cbf0a602fba01d1dc87114032a050
naomi1877 <149076597+naomi1877@users.noreply.github.com>
2025-05-20 12:23:12 +0300

    Delete step 1 directory

--- step 1/selectAll.sql
+++
@@ -1,9 +0,0 @@
-
-SELECT * FROM TaxReport;
-SELECT * FROM FinancialAudit;
-SELECT * FROM ExpenseTypes;
-SELECT * FROM Expense;
-SELECT * FROM Revenue;
-SELECT * FROM AuditRecord;
-SELECT * FROM Payment;
-SELECT * FROM Invoice;
